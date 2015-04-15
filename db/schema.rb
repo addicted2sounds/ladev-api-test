13678e2d5cddd8bef8c0e7effe411a0b4d4134ce
addicted <addicted22sounds@gmail.com>
2015-04-15 19:09:44 +0300

    users json actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150415141921) do
+ActiveRecord::Schema.define(version: 20150415151337) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.decimal  "price",       precision: 8, scale: 2
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "username",   limit: 50
